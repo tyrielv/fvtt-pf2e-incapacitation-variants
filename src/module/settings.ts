0794d7039fb01d150fa89c01af83fda92b53e67d
Tyrie Vella <tyrielv@gmail.com>
2025-04-08 11:12:10 -0700

    Update settings to use keys constant

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -1,11 +1,21 @@
 import { MODULENAME, log } from "./fvtt-pf2e-incapacitation-variants";
 
+export const Keys = {
+    HPThreshold: "HPThreshold",
+    ApplicationBasis: "ApplicationBasis",
+    RequiredLevelDifference: "RequiredLevelDifference",
+    TraitName: "TraitName",
+    SpellEffectLevel: "SpellEffectLevel",
+    IncapacitationEffect: "IncapacitationEffect",
+    BonusAmount: "BonusAmount",
+}
+
 export function registerSettings() {
     log("registerSettings");
 
-    game.settings.register(MODULENAME, "HPThreshold", {
-        name: `${MODULENAME}.SETTINGS.HPThreshold.name`,
-        hint: `${MODULENAME}.SETTINGS.HPThreshold.hint`,
+    game.settings.register(MODULENAME, Keys.HPThreshold, {
+        name: `${MODULENAME}.SETTINGS.${Keys.HPThreshold}.name`,
+        hint: `${MODULENAME}.SETTINGS.${Keys.HPThreshold}.hint`,
         scope: "world",
         config: true,
         default: 0,
@@ -17,23 +27,23 @@ export function registerSettings() {
         },
     });
 
-    game.settings.register(MODULENAME, "ApplicationBasis", {
-        name: `${MODULENAME}.SETTINGS.ApplicationBasis.name`,
-        hint: `${MODULENAME}.SETTINGS.ApplicationBasis.hint`,
+    game.settings.register(MODULENAME, Keys.ApplicationBasis, {
+        name: `${MODULENAME}.SETTINGS.${Keys.ApplicationBasis}.name`,
+        hint: `${MODULENAME}.SETTINGS.${Keys.ApplicationBasis}.hint`,
         scope: "world",
         config: true,
         default: "Level",
         type: String,
         choices: {
-            Level: game.i18n.localize(`${MODULENAME}.SETTINGS.ApplicationBasis.Level`),
-            Trait: game.i18n.localize(`${MODULENAME}.SETTINGS.ApplicationBasis.Trait`),
-            Never: game.i18n.localize(`${MODULENAME}.SETTINGS.ApplicationBasis.Never`),
+            Level: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.ApplicationBasis}.Level`),
+            Trait: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.ApplicationBasis}.Trait`),
+            Never: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.ApplicationBasis}.Never`),
         },
     });
 
-    game.settings.register(MODULENAME, "RequiredLevelDifference", {
-        name: `${MODULENAME}.SETTINGS.RequiredLevelDifference.name`,
-        hint: `${MODULENAME}.SETTINGS.RequiredLevelDifference.hint`,
+    game.settings.register(MODULENAME, Keys.RequiredLevelDifference, {
+        name: `${MODULENAME}.SETTINGS.${Keys.RequiredLevelDifference}.name`,
+        hint: `${MODULENAME}.SETTINGS.${Keys.RequiredLevelDifference}.hint`,
         scope: "world",
         config: true,
         default: 1,
@@ -45,51 +55,51 @@ export function registerSettings() {
         },
     });
 
-    game.settings.register(MODULENAME, "TraitName", {
-        name: `${MODULENAME}.SETTINGS.TraitName.name`,
-        hint: `${MODULENAME}.SETTINGS.TraitName.hint`,
+    game.settings.register(MODULENAME, Keys.TraitName, {
+        name: `${MODULENAME}.SETTINGS.${Keys.TraitName}.name`,
+        hint: `${MODULENAME}.SETTINGS.${Keys.TraitName}.hint`,
         scope: "world",
         config: true,
         default: "boss",
         type: String,
     });
 
-    game.settings.register(MODULENAME, "SpellEffectLevel", {
-        name: `${MODULENAME}.SETTINGS.SpellEffectLevel.name`,
-        hint: `${MODULENAME}.SETTINGS.SpellEffectLevel.hint`,
+    game.settings.register(MODULENAME, Keys.SpellEffectLevel, {
+        name: `${MODULENAME}.SETTINGS.${Keys.SpellEffectLevel}.name`,
+        hint: `${MODULENAME}.SETTINGS.${Keys.SpellEffectLevel}.hint`,
         scope: "world",
         config: true,
         default: "slotLevel",
         type: String,
         choices: {
-            SlotLevel: game.i18n.localize(`${MODULENAME}.SETTINGS.SpellEffectLevel.SlotLevel`),
-            CasterLevel: game.i18n.localize(`${MODULENAME}.SETTINGS.SpellEffectLevel.CasterLevel`),
-            BetterOfSlotLevelOrCasterLevel: game.i18n.localize(`${MODULENAME}.SETTINGS.SpellEffectLevel.BetterOfSlotLevelOrCasterLevel`),
+            SlotLevel: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.SpellEffectLevel}.SlotLevel`),
+            CasterLevel: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.SpellEffectLevel}.CasterLevel`),
+            BetterOfSlotLevelOrCasterLevel: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.SpellEffectLevel}.BetterOfSlotLevelOrCasterLevel`),
         },
     });
 
-    game.settings.register(MODULENAME, "IncapacitationEffect", {
-        name: `${MODULENAME}.SETTINGS.IncapacitationEffect.name`,
-        hint: `${MODULENAME}.SETTINGS.IncapacitationEffect.hint`,
+    game.settings.register(MODULENAME, Keys.IncapacitationEffect, {
+        name: `${MODULENAME}.SETTINGS.${Keys.IncapacitationEffect}.name`,
+        hint: `${MODULENAME}.SETTINGS.${Keys.IncapacitationEffect}.hint`,
         scope: "world",
         config: true,
         default: "ImproveAllDOS",
         type: String,
         choices: {
-            ImproveAllDOS: game.i18n.localize(`${MODULENAME}.SETTINGS.IncapacitationEffect.ImproveAllDOS`),
-            ImproveWorstDOS: game.i18n.localize(`${MODULENAME}.SETTINGS.IncapacitationEffect.ImproveWorstDOS`),
-            ImproveWorst2DOS: game.i18n.localize(`${MODULENAME}.SETTINGS.IncapacitationEffect.ImproveWorst2DOS`),
-            RollTwice: game.i18n.localize(`${MODULENAME}.SETTINGS.IncapacitationEffect.RollTwice`),
-            GiveBonus: game.i18n.localize(`${MODULENAME}.SETTINGS.IncapacitationEffect.GiveBonus`),
+            ImproveAllDOS: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.IncapacitationEffect}.ImproveAllDOS`),
+            ImproveWorstDOS: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.IncapacitationEffect}.ImproveWorstDOS`),
+            ImproveWorst2DOS: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.IncapacitationEffect}.ImproveWorst2DOS`),
+            RollTwice: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.IncapacitationEffect}.RollTwice`),
+            GiveBonus: game.i18n.localize(`${MODULENAME}.SETTINGS.${Keys.IncapacitationEffect}.GiveBonus`),
             ScaleDOSImprovementsWithHP: game.i18n.localize(
-                `${MODULENAME}.SETTINGS.IncapacitationEffect.ScaleDOSImprovementsWithHP`
+                `${MODULENAME}.SETTINGS.${Keys.IncapacitationEffect}.ScaleDOSImprovementsWithHP`
             ),
         },
     });
 
-    game.settings.register(MODULENAME, "BonusAmount", {
-        name: `${MODULENAME}.SETTINGS.BonusAmount.name`,
-        hint: `${MODULENAME}.SETTINGS.BonusAmount.hint`,
+    game.settings.register(MODULENAME, Keys.BonusAmount, {
+        name: `${MODULENAME}.SETTINGS.${Keys.BonusAmount}.name`,
+        hint: `${MODULENAME}.SETTINGS.${Keys.BonusAmount}.hint`,
         scope: "world",
         config: true,
         default: 5,
@@ -98,31 +108,31 @@ export function registerSettings() {
 }
 
 export function getSpellEffectLevelSetting(): SpellEffectLevel {
-    return game.settings.get(MODULENAME, "SpellEffectLevel") as SpellEffectLevel;
+    return game.settings.get(MODULENAME, Keys.SpellEffectLevel) as SpellEffectLevel;
 }
 
 export function getRequiredLevelDifferenceSetting(): number {
-    return game.settings.get(MODULENAME, "RequiredLevelDifference") as number;
+    return game.settings.get(MODULENAME, Keys.RequiredLevelDifference) as number;
 }
 
 export function getIncapacitationEffectSetting(): IncapacitationEffect {
-    return game.settings.get(MODULENAME, "IncapacitationEffect") as IncapacitationEffect;
+    return game.settings.get(MODULENAME, Keys.IncapacitationEffect) as IncapacitationEffect;
 }
 
 export function getApplicationBasisSetting(): ApplicationBasis {
-    return game.settings.get(MODULENAME, "ApplicationBasis") as ApplicationBasis;
+    return game.settings.get(MODULENAME, Keys.ApplicationBasis) as ApplicationBasis;
 }
 
 export function getBonusAmountSetting(): number {
-    return game.settings.get(MODULENAME, "BonusAmount") as number;
+    return game.settings.get(MODULENAME, Keys.BonusAmount) as number;
 }
 
 export function getHpThreshold(): number {
-    return game.settings.get(MODULENAME, "HPThreshold") as number;
+    return game.settings.get(MODULENAME, Keys.HPThreshold) as number;
 }
 
 export function getTraitNameSetting() {
-    return game.settings.get(MODULENAME, "TraitName") as string;
+    return game.settings.get(MODULENAME, Keys.TraitName) as string;
 }
 
 export type SpellEffectLevel = "SlotLevel" | "CasterLevel" | "BetterOfSlotLevelOrCasterLevel";
